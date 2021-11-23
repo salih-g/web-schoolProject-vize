abfa28c393a3b4d4e08751413095f57828a756d4
Salih Gumus <sudanmerinosu@gmail.com>
2021-11-23 12:07:08 +0300

    First

---
+++ Party/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Party.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
