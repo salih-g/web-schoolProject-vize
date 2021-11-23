a29f4ae8176c5f70b6d460b30329c90ef983ccaa
Salih Gumus <sudanmerinosu@gmail.com>
2021-11-23 13:50:50 +0300

    Pass conection string to context

---
+++ Party/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Party.Models;
+
+namespace Party.Data
+{
+	public class ApplicationDbContext:DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options){}
+
+
+		public DbSet<GuestResponse> guestResponses { get; set; }
+	}
+}
+
