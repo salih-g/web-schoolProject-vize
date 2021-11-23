f84ce09bb94368915f5b23938e1a979b635601d4
Salih Gumus <sudanmerinosu@gmail.com>
2021-11-23 14:56:15 +0300

    Get guest list to db

--- Party/Controllers/GuestController.cs
+++ Party/Controllers/GuestController.cs
@@ -1,12 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Party.Data;
+using Party.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Party.Controllers
 {
 	public class GuestController : Controller
 	{
+
+		private readonly ApplicationDbContext _db;
+
+		public GuestController(ApplicationDbContext db)
+		{
+			_db = db;
+		}
+
 		public IActionResult Index()
 		{
 			return View();
 		}
+
+		public IActionResult List()
+		{
+			IEnumerable<GuestResponse> objGuestList = _db.guestResponses;
+			return View(objGuestList);
+		}
+
+		public IActionResult Thanks()
+		{
+			return View();
+		}
+
 	}
 }
