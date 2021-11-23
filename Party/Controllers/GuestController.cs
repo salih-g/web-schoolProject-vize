a81c289ca762ee1c9727b3bbe1c2cfbaaf3d6f08
Salih Gumus <sudanmerinosu@gmail.com>
2021-11-23 14:30:14 +0300

    Guest controller and Guest Index added

---
+++ Party/Controllers/GuestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Party.Controllers
+{
+	public class GuestController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
